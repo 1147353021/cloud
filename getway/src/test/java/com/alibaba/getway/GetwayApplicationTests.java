8a2d5fc14b707d5ef5125f4d49b6412527f4b7d6
1147353021 <1147353021@qq.com>
2020-07-18 13:14:40 +0800

    add getway

---
+++ getway/src/test/java/com/alibaba/getway/GetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
